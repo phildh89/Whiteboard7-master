795e56dd43c4ab84b3eca6145539e2a44a3448a1
phildh89 <phildh89@gmail.com>
2020-02-18 13:50:25 -0800

    DistinctArray and Reverse Project

---
+++ DistinctCountArray/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace DistinctCountArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DistinctCountArray.Program.Main()");
+
+            int[] example = { 2, 3, 5, 6, 2 };
+
+
+            Console.WriteLine(DistinctCount(example));
+        }
+        public static int DistinctCount(int[] inputArray)
+        {
+            int count = 0;
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                bool isNew = true;
+                for (int k = 0; k < i; k++)
+                {
+                    if (inputArray[k] == inputArray[i])
+                    {
+                        isNew = false;
+                    }
+
+                }
+                if(isNew == true)count++;
+
+            }
+
+
+            return count;
+        }
+
+
+    }
+}
---
+++ ReverseInt/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ReverseInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ReverseInt.Program.Main()");
+            Reverse(10, 15);
+        }
+        public static void Reverse(int x, int y)
+        {
+            y = x + y;
+            x = y - x;
+            y = y - x;
+
+
+
+            Console.WriteLine($"x={x}, y={y}");
+        }
+    }
+}
