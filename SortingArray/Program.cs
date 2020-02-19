fb2fd38f3a17670dd86e6d0fda8b3c5eb59cd2be
phildh89 <phildh89@gmail.com>
2020-02-19 10:53:34 -0800

    Added Project SortingArray

---
+++ SortingArray/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SortingArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SortingArray.Porgram.Main()");
+
+            int[] testArray = { 4, 1, 6, 5, 1, 6, 2, 5 };
+            PrintArray(testArray);
+            Console.WriteLine("\n");
+            PrintArray(SortArray(testArray));
+        }
+        public static void PrintArray(int[] arrPrint)
+        {
+            foreach (var p in arrPrint)
+            {
+                Console.Write(p);
+            }
+        }
+
+        public static int[] SortArray(int[] arr)
+        {
+            int temp = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int k = i+1; k < arr.Length; k++)
+                {
+                    if (arr[i] > arr[k])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[k];
+                        arr[k] = temp;
+                    }
+                }
+            }
+            return arr;
+        }
+    }
+}
