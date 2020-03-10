e6f9781e7b3b3ec925b427e82935962ddc0ffff2
phildh89 <phildh89@gmail.com>
2020-03-10 11:21:17 -0700

    Add CountCapitalLetters Program

---
+++ CountCapitalLetters/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CountCapitalLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CountCapitalLetters.Program.Main()");
+
+            Console.WriteLine(CountCap("Welcome To The Jungle"));
+        }
+        public static int CountCap(string input)
+        {
+            int count = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string temp = input.ToLower();
+                if (input[i] != temp[i])
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
